dfe0b592ae70d4930ef039e23f2c085f43d8ff9c
Mayko Diouzef <maykodiouzef+github@gmail.com>
2025-11-13 03:18:10 -0300

    Update Peca.java

--- projetofinaljava/src/projetofinaljava/Peca.java
+++ projetofinaljava/src/projetofinaljava/Peca.java
@@ -3,7 +3,7 @@ package projetofinaljava;
 import java.util.Scanner;
 
 /**
- * Classe responsavel por gerenciar matriz lista que contem as peças
+ * Classe responsável por gerenciar matriz lista de peças.
  *
  * @author Mayko Amaral
  */
@@ -11,15 +11,15 @@ public class Peca {
 
     static String nome;
     static int codigo = 0;
-    static String[][] listaPecas = new String[10][2];
+    static String[][] listaPecas = new String[100][2];
 
     /**
-     * Este metodo cadastra uma nova peça na matriz de peças.
+     * Este metodo responsável por cadastrar uma nova peça na matriz listaPecas.
      */
     public static void Cadastrar() {
 
         System.out.println("--------------------------------------");
-        System.out.print("=> Nome da Peca: ");
+        System.out.print("=> Nome da peca: ");
         Scanner lsNome = new Scanner(System.in);
         nome = lsNome.nextLine();
 
@@ -31,5 +31,26 @@ public class Peca {
         System.out.println("=> A peca " + nome + " foi cadastrada com sucesso!");
         System.out.println("--------------------------------------");
 
+    }
+
+    /**
+     * Este metodo responsável por listar a matriz listaPecas.
+     */
+    public static void Listar() {
+        
+        System.out.println("Codigo\t\tPeca");
+
+        for (int linha = 0; linha < listaPecas.length; linha++) {
+
+            if (listaPecas[linha][0] != null) {
+                for (int coluna = 0; coluna < listaPecas[linha].length; coluna++) {
+                    System.out.print(listaPecas[linha][coluna] + "\t\t");
+                }
+
+                System.out.println("");
+                System.out.println("--------------------------------------");
+            }
+
+        }
     }
 }
